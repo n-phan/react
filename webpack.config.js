cf74ac299bacf4db1d7054cfec6aff25ef8817ef
Nathan Phan <nathan.hd.phan@gmail.com>
2020-06-04 11:16:36 -0700

    2 Setup

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require("path");
+
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
+
+var config = {
+    entry: SRC_DIR + '/app/index.js',
+    output: {
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?/,
+                include: [SRC_DIR],
+                loader: "babel-loader",
+                options: {
+                    presets: ["es2015", "react", "stage-2"]
+                }
+            }
+        ]
+    }
+};
